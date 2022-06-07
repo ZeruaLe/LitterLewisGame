8f87173a3a0bf1d12cec202aca1287dd46581e3a
AHHHrx <AHHHrx.work@gmail.com>
2022-06-07 21:08:35 +1000

    vsjgvbdfa

--- Unity file Litter/Assets/Scripts/EnemyMovement.cs
+++ Unity file Litter/Assets/Scripts/EnemyMovement.cs
@@ -10,13 +10,16 @@ public class EnemyMovement : MonoBehaviour
     public Transform groundDetection;
     public LayerMask wallMask;
     public LayerMask patrolMask;
+    public LayerMask groundMask;
 
     // Runs every frame
     void Update()
     {    
+        //int wallMask = LayerMask.GetMask("Floor");  // Sets the variable for the "Floor" layer
+        //int patrolMask = LayerMask.GetMask("Waypoint"); // Sets variable for the "Waypoint" layer
         transform.Translate(Vector2.right * speed * Time.deltaTime);    // Enemy moves forward
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);  // Sets variable to see if there is a floor to walk across below the groundDetection
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, groundMask);  // Sets variable to see if there is a floor to walk across below the groundDetection
         RaycastHit2D wallInfo = Physics2D.Raycast(groundDetection.position, Vector2.zero, wallMask);    // Sets variable to see if there is a wall inside groundDetection
         RaycastHit2D patrolInfo = Physics2D.Raycast(groundDetection.position, Vector2.zero, patrolMask);    // Sets variable to see if theres a waypoint inside groundDetection
 
