f190eb06f732b49682da7246f196fa0310cf6df4
AHHHrx <AHHHrx.work@gmail.com>
2022-06-14 20:09:45 +1000

    stop sound util

--- Unity file Litter/Assets/Scripts/Sounds/LitterSoundPlayer.cs
+++ Unity file Litter/Assets/Scripts/Sounds/LitterSoundPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LitterSoundPlayer : MonoBehaviour
 {
     public bool playOnEnable = false;
+    public bool stopOnDisable = false;
     public string soundToPlay;
 
     private void OnEnable()
@@ -13,8 +14,19 @@ public class LitterSoundPlayer : MonoBehaviour
             Play();
     }
 
+    private void OnDisable()
+    {
+        if (stopOnDisable)
+            Stop();
+    }
+
     public void Play()
     {
         SoundManagerScript.instance.Play(soundToPlay);
+    }
+
+    public void Stop()
+    {
+        SoundManagerScript.instance.Stop(soundToPlay);
     }
 }
